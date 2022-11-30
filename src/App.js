d0ed17fe1344904245c9f6e919717d2d53a9d154
ShivaTeja <vddshivateja@gmail.com>
2022-11-30 06:03:18 +0000

    second  commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Welcome from './components/Welcome'
+import Counter from './components/Counter'
+
+const App = () => {
+  return (
+    <div>
+      <Counter />
+    </div>
+  )
+}
+export default App
